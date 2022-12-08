61734dc4521d688497b28cda413355de57f768ab
Bajarang2002 <120096751+Bajarang2002@users.noreply.github.com>
2022-12-08 16:00:02 +0530

    addition of two number

---
+++ addition .c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a,b,c;
+  clrscr();
+  a=5;
+  b=6;
+  c=a+b;
+  {
+    printf("addition:%d",c);
+    
+  }
+getch();
+}
+   
+    
+  
+  
