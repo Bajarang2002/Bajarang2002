7937bc55eee77215778d8e55002342b016d7f33f
Bajarang2002 <120096751+Bajarang2002@users.noreply.github.com>
2022-12-25 17:14:44 +0530

    Accept two number and display the number in the range to that number

---
+++ for.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i,num;
+    printf("Enter the number:");
+    scanf("%d",&num);
+    for(i=0;i<=num;i++)
+    {
+     printf("\n %d",i);
+    }
+    return 0;
+}
+      
