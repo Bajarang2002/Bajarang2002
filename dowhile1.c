f763b95189ac2dc2177053cf2681a94b59389613
Bajarang2002 <120096751+Bajarang2002@users.noreply.github.com>
2022-12-28 16:43:11 +0530

    Accept number from user and display no user ent 0

---
+++ dowhile1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    do
+    {
+    printf("Enter the number:");
+    scanf("%d",&num);
+    printf("\n%d",num);
+    }
+    while(num!=0);
+    return 0;
+}
+    
